c425605a5882380df96ee58a676ecff7c8f6f38f
Rakshit Rawat <sayamr086@gmail.com>
2025-06-02 14:54:16 +0530

    add: remaining grid item in the Blog

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -14,9 +14,9 @@ const Blog = () => {
         </h2>
       </div>
 
-
-          {/* Blog Grid */}
+      {/* Blog Grid */}
       <div className="w-full grid grid-cols-[585px_1fr] grid-rows-[285px_1fr] gap-8 h-[600px] mb-28">
+        {/* LEFT COLUMN - Tall card, spans 2 rows */}
         <div className="bg-black  row-span-2 rounded-2xl bg-rider bg-center relative ">
           <div className="flex gap-2 top-10 left-10 absolute">
             <button className="text-black font-barlow text-[14px] uppercase items  bg-[#bdff6a] rounded-xl px-3  ">
@@ -33,13 +33,14 @@ const Blog = () => {
           </div>
         </div>
 
+        {/* RIGHT COLUMN, FIRST ROW */}
         <div className="bg-black rounded-2xl bg-jumper bg-no-repeat bg-center bg-cover relative">
           <div className="flex gap-2 top-7 left-9 absolute">
             <button className="text-black font-barlow text-[14px] uppercase items  bg-[#bdff6a] rounded-xl px-3  ">
               Sports
             </button>
             <h2 className="text-white mx-1 py-[2px] font-normal text-[.91rem] ">
-              JUNER 21 2026
+              JUNE 21 2026
             </h2>
           </div>
           <div className="absolute  top-[210px] left-8 line-clamp-1">
@@ -47,7 +48,47 @@ const Blog = () => {
               The Cycling Guide to senior Gravel amanda
             </h1>
           </div>
-          
+        </div>
+
+        {/* RIGHT COLUMN, SECOND ROW  */}
+
+        <div className="grid grid-cols-2 gap-8 ">
+
+          <div className=" rounded-2xl bg-cover bg-center bg-firstPerson h-full relative">
+            <div className="absolute inset-0 bg-black/25 z-0 rounded-2xl" />
+            <div className="flex gap-2 top-7 left-9 absolute">
+              <button className="text-black font-barlow text-[14px] uppercase items  bg-[#bdff6a] rounded-xl px-3  ">
+                Sports
+              </button>
+              <h2 className="text-white mx-1 py-[2px] font-normal text-[.91rem] ">
+                JULY 5 2024
+              </h2>
+            </div>
+            <div className="absolute  top-[210px] left-8 ">
+              <h1 className="text-white font-barlow font-semibold text-[20px] text-wrap uppercase">
+                The Cycling Guide to senior Gravel amanda
+              </h1>
+            </div>
+          </div>
+        
+          <div className=" rounded-2xl bg-cover bg-center bg-secondPerson h-full relative">
+             <div className="absolute inset-0 bg-black/25 z-0 rounded-2xl" />
+            <div className="flex gap-2 top-7 left-9 absolute">
+              <button className="text-black font-barlow text-[14px] uppercase items  bg-[#bdff6a] rounded-xl px-3  ">
+                Sports
+              </button>
+              <h2 className="text-white mx-1 py-[2px] font-normal text-[.91rem] ">
+                AUGUST 10 2025
+              </h2>
+            </div>
+
+            <div className="absolute  top-[210px] left-8 ">
+              <h1 className="text-white font-barlow font-semibold text-[20px] uppercase">
+                The Cycling Guide to senior Gravel amanda
+              </h1>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,13 @@ export default {
         card: "url(https://cdn.shopify.com/s/files/1/0577/9675/5633/files/leo_daone_home1_card.png?v=1702981001)",
         rider:
           "url(https://cdn.shopify.com/s/files/1/0577/9675/5633/files/leo_daone_home1_blog_1.jpg?v=1703124490)",
-          jumper:"url(https://cdn.shopify.com/s/files/1/0577/9675/5633/files/leo_daone_home1_blog_2.jpg?v=1703124490)"
-      },
-      rotate: {
+        jumper:
+          "url(https://cdn.shopify.com/s/files/1/0577/9675/5633/files/leo_daone_home1_blog_2.jpg?v=1703124490)",
+        firstPerson:
+          "url(https://demo80leotheme.b-cdn.net/prestashop/leo_daone_elementor_demo/img/leoblog/b/1/6/1200_800/b-blog4.jpg)",
+        secondPerson:
+          "url(https://demo80leotheme.b-cdn.net/prestashop/leo_daone_elementor_demo/img/leoblog/b/1/5/1200_800/b-blog5.jpg)",
+        rotate: {},
         "0%": { transform: "rotate(0deg)" },
         "360%": { transform: "rotate(360deg)" },
       },
